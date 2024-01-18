62fd971a23490cd446d4d0efabc3539c7b18e647
Santiago <andressasi@unisabana.edu.co>
2024-01-18 00:15:01 -0500

    Add navigation bar and images

--- src/app/Components/nav.js
+++ src/app/Components/nav.js
@@ -1,24 +1,18 @@
-
+import Link from 'next/link';
 const Nav = () => {
   return (
     <div>
-      <ul>
-          <li>
-            <button onClick={() => navigateToSection('section1')}>
-              Section 1
-            </button>
-          </li>
-          <li>
-            <button onClick={() => navigateToSection('section2')}>
-              Section 2
-            </button>
-          </li>
-          <li>
-            <button onClick={() => navigateToSection('section3')}>
-              Section 3
-            </button>
-          </li>
-        </ul>
+        <nav>
+            <img src="/LogoWebelopers.png" alt="Logo" />
+            <Link href="#section1">Inicio</Link>
+            <Link href="#section2">Servicios</Link>
+            <Link href="#section3">Quienes Somos</Link>
+            <Link href="#section4">Proyectos</Link>
+            <Link href="#section5">Contactanos</Link>
+            <img className="sol" src="/solecito.png" alt="Modo Claro" />
+            <img className="ingles" src="/Ingles.png" alt="English" />
+            <button>Cotiza ahora</button>
+        </nav>
     </div>
   );
 }
