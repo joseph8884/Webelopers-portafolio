e19b3727ff5f166d95179d1baeef46932a314a0a
Giovanni Esteban Moreno Urbina <ingestebanmu@hotmail.com>
2024-03-01 16:58:42 -0500

    gio 02

--- src/app/Components/Gio.jsx
+++ src/app/Components/Gio.jsx
@@ -3,44 +3,50 @@ import Image from "next/image";
 
 const Gio = () => {
   return (
-    <section className="quienes_somos_carrusel">
-      <div className="grid grid-cols-2 gap-4">
-        <div>
-          <Image src="/GioWebo.png" alt="Image 1" width={100} height={100} />
-        </div>
-        <div>
-          <Image src="/GioWebo.png" alt="Image 1" width={100} height={100} />
-        </div>
-        <div>
-          <Image src="/GioWebo.png" alt="Image 1" width={100} height={100} />
-        </div>
-        <div>
-          <Image src="/GioWebo.png" alt="Image 1" width={100} height={100} />
-        </div>
+    <section className="parent">
+      <div className="textoGrid">
+        <h2>Quienes Somos </h2>
       </div>
+      <div class="fotosGrid">
+        <img src="/GioWebo.png" alt="Image 1" className="active" />
+        <img src="/GioWebo.png" alt="Image 1" />
+        <img src="/GioWebo.png" alt="Image 1" />
+        <img src="/GioWebo.png" alt="Image 1" />
+      </div>
+      <div className="cardGrid">
+        <img src="/GioWebo.png" alt="Image 1" />
 
-      <div className="flex flex-col rounded-lg bg-white dark:bg-white dark:text-black shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] md:max-w-xl md:flex-row p-8">
-        <Image
-          src="/GioWebo.png"
-          alt="Celular"
-          width={100}
-          height={20}
-          layout="responsive"
-        />
-        <div class="flex flex-col justify-start p-6">
-          <h5 class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-800">
+        <div className="flex flex-col justify-start p-6">
+          <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-800">
             Gio
           </h5>
-          <p class="mb-4 text-base text-black dark:text-black">
+          <p className="mb-4 text-base text-black dark:text-black">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto
             labore magnam mollitia nulla, incidunt eligendi accusamus illo
             quaerat commodi repellendus suscipit culpa. Dolores exercitationem
             repellat eos asperiores incidunt voluptas nam.
           </p>
-          <p class="text-xs text-neutral-500 dark:text-neutral-300">
+          <p className="text-xs text-neutral-500 dark:text-neutral-300">
             Last updated 3 mins ago
           </p>
         </div>
+      </div>
+      <div className="changeCard">
+        <button className="change">
+          <img src="/webitoBorde.png" alt="Image 1" />
+        </button>
+        <button className="change">
+          <img src="/webitoBorde.png" alt="Image 1" />
+        </button>
+        <button className="change">
+          <img src="/webitoBorde.png" alt="Image 1" />
+        </button>
+        <button className="change">
+          <img src="/webitoBorde.png" alt="Image 1" />
+        </button>
+        <button className="change">
+          <img src="/webitoBorde.png" alt="Image 1" />
+        </button>
       </div>
     </section>
   );
